bfeb7de2c370b9ad13d6c53ac91960522bb95837
TheOneTold <74574842+TheOneTold@users.noreply.github.com>
2026-09-16 23:43:25 -0500

    Typo fix

--- minecraft/net/minecraft/src/CraftingManager.java
+++ minecraft/net/minecraft/src/CraftingManager.java
@@ -96,9 +96,9 @@ public class CraftingManager {
 		this.addRecipe(new ItemStack(Block.flintBricks, 2), new Object[]{"##", "##", Character.valueOf('#'), Item.flint});
 		this.addRecipe(new ItemStack(Block.clayBricks, 4), new Object[]{"##", "##", Character.valueOf('#'), Block.blockClay});
 		this.addRecipe(new ItemStack(Block.blockClay, 1), new Object[]{"#", Character.valueOf('#'), Block.clayBricks});
-		this.addRecipe(new ItemStack(Block.stairShingles, 3), new Object[]{"#  ", "## ", Character.valueOf('#'), item.flint});
+		this.addRecipe(new ItemStack(Block.stairShingles, 3), new Object[]{"#  ", "## ", Character.valueOf('#'), Item.flint});
 		this.addRecipe(new ItemStack(Block.stairTiles, 4), new Object[]{"#  ", "## ", "###", Character.valueOf('#'), Block.tileBricks});
-		this.addRecipe(new ItemStack(Block.slabShinglesSingle, 1), new Object[]{"##", Character.valueOf('#'), item.flint});
+		this.addRecipe(new ItemStack(Block.slabShinglesSingle, 1), new Object[]{"##", Character.valueOf('#'), Item.flint});
 		this.addRecipe(new ItemStack(Block.slabTileSingle, 3), new Object[]{"###", Character.valueOf('#'), Block.tileBricks});
 		this.addRecipe(new ItemStack(Block.whiteConcrete, 8), new Object[]{"X#X", "#Y#", "X#X", Character.valueOf('X'), Item.gunpowder, Character.valueOf('Y'), Item.bucketWater, Character.valueOf('#'), Block.sandStone});
 		this.addRecipe(new ItemStack(Block.blackConcrete, 8), new Object[]{"X#X", "#Y#", "X#X", Character.valueOf('X'), Item.gunpowder, Character.valueOf('Y'), Item.bucketWater, Character.valueOf('#'), Block.stone});
